6d59999a37b8d08024a29ef5464b3bfe199db993
ypavanr <ypavanreddy190@gmail.com>
2025-05-12 23:47:10 +0530

    slab calculation update

--- frontend/src/About.jsx
+++ frontend/src/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
           <ul>
             <li><a href="#">Home</a></li>
             <li><a href="#">About us</a></li>
-            <li><a href="#">Login</a></li>
+            <li><a href="/login">Login</a></li>
           </ul>
         </nav>
       </header>
--- slab_calc.js
+++ slab_calc.js
@@ -1,52 +1,88 @@
 function taxToPay(taxableAmount){
-    var tax=0
+   var taxAmount=0;
+   var tax={
+      "slab1":[0,0],
+      "slab2":[0,0],
+      "slab3":[0,0],
+      "slab4":[0,0],
+      "slab5":[0,0],
+      "slab6":[0,0],
+      "slab7":[0,0]
+   };
     if(taxableAmount<=400000){
+      tax.slab1[0]=taxableAmount;
+      tax.slab1[1]=0;
         return tax;
     }
     else{
+      tax.slab1[0]=400000;
+      tax.slab1[1]=0;
      taxableAmount=taxableAmount-400000;
      if(taxableAmount<=400000){
-        tax+= Math.round(taxableAmount*(5/100))
+      tax.slab1[0]=taxableAmount;
+      tax.slab1[1]=0;
+        taxAmount+= Math.round(taxableAmount*(5/100))
         return tax;
      }
      else{
-        tax=tax+ Math.round(400000*(5/100))
+        tax.slab2[0]=400000;
+        taxAmount=taxAmount+ Math.round(400000*(5/100))
+        tax.slab2[1]=taxAmount;
         taxableAmount=taxableAmount-400000;
      }
      if(taxableAmount<=400000){
-        tax+= Math.round(taxableAmount*(10/100))
+      tax.slab3[0]=taxableAmount;
+        taxAmount+= Math.round(taxableAmount*(10/100))
+        tax.slab3[1]=taxAmount;
         return tax;
      }
      else{
-        tax=tax+ Math.round(400000*(10/100))
+      tax.slab3[0]=400000;
+        taxAmount=taxAmount+ Math.round(400000*(10/100))
+        tax.slab3[1]=taxAmount;
         taxableAmount=taxableAmount-400000;
      }
      if(taxableAmount<=400000){
-        tax+= Math.round(taxableAmount*(15/100))
+      tax.slab4[0]=taxableAmount;
+        taxAmount+= Math.round(taxableAmount*(15/100))
+        tax.slab4[1]=taxAmount;
         return tax;
      }
      else{
-        tax=tax+ Math.round(400000*(15/100))
+      tax.slab4[0]=400000;
+        taxAmount=taxAmount+ Math.round(400000*(15/100))
+        tax.slab4[1]=taxAmount;
         taxableAmount=taxableAmount-400000;
      }
      if(taxableAmount<=400000){
-        tax+= Math.round(taxableAmount*(20/100))
+      tax.slab5[0]=taxableAmount;
+        taxAmount+= Math.round(taxableAmount*(20/100))
+        tax.slab5[1]=taxAmount;
         return tax;
      }
      else{
-        tax=tax+ Math.round(400000*(20/100))
+      tax.slab5[0]=400000;
+        taxAmount=taxAmount+ Math.round(400000*(20/100))
+        tax.slab5[1]=taxAmount;
         taxableAmount=taxableAmount-400000;
      }
      if(taxableAmount<=400000){
-        tax+= Math.round(taxableAmount*(25/100))
+      tax.slab6[0]=taxableAmount;
+        taxAmount+= Math.round(taxableAmount*(25/100))
+        tax.slab6[1]=taxAmount;
         return tax;
      }
      else{
-        tax=tax+ Math.round(400000*(25/100))
+      tax.slab6[0]=400000;
+        taxAmount=taxAmount+ Math.round(400000*(25/100))
+        tax.slab6[1]=taxAmount;
         taxableAmount=taxableAmount-400000;
      }
      if(taxableAmount>0)
-    {tax=tax+ Math.round(taxableAmount*(30/100))}
+    {tax.slab7[0]=taxableAmount;
+      taxAmount=taxAmount+ Math.round(taxableAmount*(30/100))
+      tax.slab7[1]=taxAmount;
+    }
      return tax
     }
 }
