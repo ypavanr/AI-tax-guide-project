a8a034b63c2ce5a2ba57c74e4cc8157288cc1696
ypavanr <ypavanreddy190@gmail.com>
2025-05-15 00:45:21 +0530

    backend creation

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import bodyParser from "body-parser";
+import db from "./config/pgDB.js";
+import cors from "cors";
+import env from "dotenv";
+const app=express()
+env.config()
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors({
+    origin:process.env.CORS_ORIGIN_URL
+}));
+db.connect().then(()=>{
+  console.log("connected to database")
+})
+app.listen(3000,()=>{
+    console.log("server listening on port 3000")
+})
